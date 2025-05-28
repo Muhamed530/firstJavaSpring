623352b8a80b51769f2f6b39826a6969999f0646
Muhamed Ahmad <Muhamed.ahmad530@gmail.com>
2025-05-27 22:14:09 -0400

    first commit

---
+++ src/main/java/com/example/firstJavaSpring/FirstJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstJavaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/firstJavaSpring/FirstJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstJavaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
